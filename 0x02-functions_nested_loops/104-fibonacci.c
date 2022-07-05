48365daaa1bb5435336518c3c40aea159e67b798
Romha Shwangzaw <romhas@gmail.com>
2022-07-05 11:10:12 -0700

    Task 16 done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long double a, b, c, i;
+
+	a = 1;
+	b = 2;
+	printf("%Lf, %Lf", a, b);
+	for (i = 2; i < 98; i++)
+	{
+		c = a + b;
+		printf(", %Lf", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
