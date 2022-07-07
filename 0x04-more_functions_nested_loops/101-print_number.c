ff84bad05d17d9a30ce38b85ea0824a29a121afb
Romha Shwangzaw <romhas@gmail.com>
2022-07-07 12:03:18 -0700

    Task 12 fourth correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -27,18 +27,17 @@ int reverse_digits(int n)
 void print_number(int n)
 {
 	int rev;
-	char c;
 
-	c = '-';
 	if (n == 0)
 		_putchar(n % 10 + '0');
 
 	rev = reverse_digits(n);
-	if (rev < 0)
+	if (n < 0)
 	{
+		_putchar('-');
 		rev = -rev;
-		_putchar(c);
 	}
+
 	while (rev != 0)
 	{
 		_putchar(rev % 10 + '0');
