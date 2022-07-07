ddd50cfdcceadf653ec279d741adb74a59fbcb7c
Romha Shwangzaw <romhas@gmail.com>
2022-07-07 12:47:42 -0700

    Task 12 fifth correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,19 @@
 #include "main.h"
 
 /**
- * reverse_digits - reverses the digits of an integer.
- * @n: The integer to be reversed.
- * Return: The reversed integer.
- */
-int reverse_digits(int n)
-{
-	int rem, reverse;
-
-	reverse = 0;
-	while (n != 0)
-	{
-		rem = n % 10;
-		reverse = reverse * 10 + rem;
-		n = n / 10;
-	}
-
-	return (reverse);
-}
-
-/**
  * print_number - prints an integer, using only _putchar()
  * @n: The integer to be printed
  */
 void print_number(int n)
 {
-	int rev;
-
-	if (n == 0)
-		_putchar(n % 10 + '0');
-
-	rev = reverse_digits(n);
 	if (n < 0)
 	{
 		_putchar('-');
-		rev = -rev;
+		n = -n;
 	}
 
-	while (rev != 0)
-	{
-		_putchar(rev % 10 + '0');
-		rev = rev / 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
 }
