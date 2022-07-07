974364ed5949b059516640d34a71d296b293b37c
Romha Shwangzaw <romhas@gmail.com>
2022-07-07 00:51:49 -0700

    Task 5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ */
+void more_numbers(void)
+{
+	int c, i;
+
+	c = 0;
+	for (i = 0; i < 10; i++)
+	{
+		while (c <= 14)
+		{
+			if (c > 9)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			c++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 2-mul.c multiplies two integers.
 3-print_numbers.c prints the numbers, from 0 to 9, followed by a new line.
 4-print_most_numbers.c prints the numbers, from 0 to 9, except 2 and 4, followed by a new line.
+5-more_numbers.c prints 10 times the numbers, from 0 to 14, followed by a new line.
