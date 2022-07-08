56d81d6047869cb15408bb12d2eb9c22774353e9
Romha Shwangzaw <romhas@gmail.com>
2022-07-08 00:05:16 -0700

    Task 12 eleventh correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_number - prints an integer, using only _putchar()
@@ -6,14 +7,27 @@
  */
 void print_number(int n)
 {
+	char last_digit;
+
+	last_digit = '0';
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n == INT_MIN)
+		{
+			last_digit = (n % -10) + '0';
+			n = -n / 10;
+		}
+		else
+		{
+			n = -n;
+		}
 	}
 
 	if (n / 10)
 		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
+	if (last_digit != '0')
+		_putchar(last_digit);
 }
