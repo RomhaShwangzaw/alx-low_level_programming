fc8faf5271a6eb13ca9e76a4ae019e8612337a14
Romha Shwangzaw <romhas@gmail.com>
2022-07-11 09:22:42 -0700

    Task 10 done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int i, j, begin, end, num, count, tmp_sum, tmp_count;
+
+	count = 0;
+	num = 0;
+	end = _strlen(s) - 1;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			begin = i;
+			for (; i < _strlen(s); i++)
+			{
+				if (s[i] < '0' || s[i] > '9')
+				{
+					end = i - 1;
+					break;
+				}
+				count++;
+			}
+			break;
+		}
+	}
+
+	for (i = begin; i <= end; i++)
+	{
+		tmp_sum = s[i] - '0';
+		tmp_count = count;
+		for (j = 0; j < tmp_count - 1; j++)
+			tmp_sum *= 10;
+		count--;
+		num += tmp_sum;
+	}
+	if (s[begin - 1] == '-')
+		num = -num;
+
+	return (num);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: The string, whose length will be calculated.
+ * Return: The length of s.
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
