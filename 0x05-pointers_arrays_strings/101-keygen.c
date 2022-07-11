bc043047c2ed44a7e5a1abd0c225859b3c0f03ba
Romha Shwangzaw <romhas@gmail.com>
2022-07-11 13:04:57 -0700

    Task 11 fourth correction

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,44 +8,15 @@
  */
 void random_password_generator(void)
 {
-	int i = 0;
-	int n = 10;
-	int randomizer = 0;
-	char numbers[] = "0123456789";
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-	char symbols[] = "!@#$^&*?";
-	char password[10];
+	int i;
+	char password[12];
 
 	srand((unsigned int)(time(NULL)));
-	randomizer = rand() % 4;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 12; i++)
 	{
-		if (randomizer == 1)
-		{
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
+		password[i] = 33 + rand() % 94;
+		printf("%c", password[i]);
 	}
 }
 
