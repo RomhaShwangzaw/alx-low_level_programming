973e955e72d91b7a139860b2f48bf035da682c0c
Romha Shwangzaw <romhas@gmail.com>
2022-07-11 03:24:35 -0700

    Task 0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of parameter to 98.
+ * @n: The parameter whose dereferenced value will be changed.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
