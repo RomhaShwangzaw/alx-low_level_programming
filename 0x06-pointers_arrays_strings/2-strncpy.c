30342f4ad8c9a52787fdc382fb938d1d573fae70
Romha Shwangzaw <romhas@gmail.com>
2022-07-13 08:31:39 -0700

    Task 2 minor correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,6 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	if (_strlen(src) == 0)
+		return (dest);
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		*(dest + i) = *(src + i);
 	for ( ; i < n; i++) 
