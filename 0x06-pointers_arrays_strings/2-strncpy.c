d82119e40da5395a606e0f71914f5c514a53b972
Romha Shwangzaw <romhas@gmail.com>
2022-07-13 09:27:50 -0700

    Task 2 sixth correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strncpy - copies one string to another.
@@ -11,14 +10,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
-	if (dest == NULL)
-		return NULL;
+	char *start = dest;
 
 	for (i = 0; i < n && *(src + i) != '\0'; i++)
 		*(dest + i) = *(src + i);
 	for ( ; i < n; i++)
 		*(dest + i) = '\0';
 
-	return (dest);
+	return (start);
 }
