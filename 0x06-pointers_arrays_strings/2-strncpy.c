8d86fff542de5f1fdcafdf949f758ae6b8bd5462
Romha Shwangzaw <romhas@gmail.com>
2022-07-13 01:54:36 -0700

    Task 2 done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies one string to another.
+ * @dest: The string to be copied to.
+ * @src: The string to be copied.
+ * @n: The number of bytes to be copied.
+ * Return: the dest pointer.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = *(src + i);
+	for ( ; i < n; i++) 
+		*(dest + i) = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: The string, whose length will be calculated.
+ * Return: The length of s.
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,5 @@
 concatenates two strings.
 # 1-strncat.c
 concatenates two strings, with specific number of bytes from source.
+# 2-strncpy.c
+copies a string, with specific number of bytes from source.
