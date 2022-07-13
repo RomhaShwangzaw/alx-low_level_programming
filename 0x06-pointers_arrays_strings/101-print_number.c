91544cfd3c66f06d09da6cca3e06b8ae62c02905
Romha Shwangzaw <romhas@gmail.com>
2022-07-13 13:54:48 -0700

    Task 9 done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * print_number - prints an integer, using only _putchar()
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	char last_digit;
+
+	last_digit = '0';
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == INT_MIN)
+		{
+			last_digit = -(n % 10) + '0';
+			n = -n / 10;
+		}
+		else
+		{
+			n = -n;
+		}
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+	if (last_digit != '0')
+		_putchar(last_digit);
+}
