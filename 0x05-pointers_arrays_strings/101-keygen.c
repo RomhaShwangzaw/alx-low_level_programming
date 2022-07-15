65c4f006d6a7c62d0fef8d27a624298f6104bdbc
Romha Shwangzaw <romhas@gmail.com>
2022-07-15 12:38:51 -0700

    Task 11 fifth correction

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,32 +1,35 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * random_password_generator - randomly generates password of length N
- */
-void random_password_generator(void)
-{
-	int i;
-	char password[12];
-
-	srand((unsigned int)(time(NULL)));
-
-	for (i = 0; i < 12; i++)
-	{
-		password[i] = 33 + rand() % 94;
-		printf("%c", password[i]);
-	}
-}
-
-/**
- * main - Entry point
- * Return: Always 0(Success)
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	random_password_generator();
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
 	return (0);
 }
