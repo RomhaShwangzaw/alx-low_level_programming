6c4a7d49ecc78bf9dffedbe70c9e24ec65832289
Romha Shwangzaw <romhas@gmail.com>
2022-07-15 13:16:15 -0700

    Task 16 fourth correction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,37 @@
  */
 int main(void)
 {
-	long double a, b, c, i;
+	unsigned long int a, b, c, i, a1, a2, b1, b2, c1, c2, l;
 
+	l = 1000000000;
 	a = 1;
 	b = 2;
-	printf("%.0Lf, %.0Lf", a, b);
-	for (i = 2; i < 98; i++)
+
+	printf("%lu, %lu", a, b);
+
+	for (i = 2; i < 92; i++)
 	{
 		c = a + b;
-		printf(", %.0Lf", c);
+		printf(", %lu", c);
 		a = b;
 		b = c;
+	}
+	
+	a1 = a / l;
+	a2 = a % l;
+	b1 = b / l;
+	b2 = b %l;
+
+	for (i = 92; i < 98; i++)
+	{
+		c1 = a1 + b1;
+		c2 = a2 + b2;
+		printf(", %lu", c1);
+		printf("%lu", c2);
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
 	}
 	printf("\n");
 
