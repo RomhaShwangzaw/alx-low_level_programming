96f43d07f75a441e88a64ca5c628826b26beb507
Romha Shwangzaw <romhas@gmail.com>
2022-07-18 03:17:23 -0700

    Task 2 second correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string.
@@ -20,5 +19,5 @@ char *_strchr(char *s, char c)
 	if (*s == '\0' && *s == c)
 		return (s);
 
-	return (NULL);
+	return (0);
 }
