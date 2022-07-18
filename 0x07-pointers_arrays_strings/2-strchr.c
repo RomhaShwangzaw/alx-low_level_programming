3c3eb1e0c09db6bfffe308b26917e4b214b9c300
Romha Shwangzaw <romhas@gmail.com>
2022-07-18 03:00:44 -0700

    Task 2 done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: The string.
+ * @c: The character to be searched.
+ * Return: a pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,5 @@
 fills memory with a constant byte.
 # 1-memcpy.c
 copies memory area.
+# 2-strchr.c
+locates a character in a string.
