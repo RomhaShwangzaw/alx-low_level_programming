5e919763aeff08eb7f272c25b885e274d397a411
Romha Shwangzaw <romhas@gmail.com>
2022-07-22 07:54:20 -0700

    Task 3 minor correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,14 +3,14 @@
 
 /**
  * main - multiplies two numbers and displays the result.
- * @argc: Then number of arguments.
+ * @argc: The number of arguments.
  * @argv: an array containing all the arguments.
  *
  * Return: 0 (Success), or 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
