ce7febdd8cdc6154f7a180f67d449b90b5c6fdd8
Romha Shwangzaw <romhas@gmail.com>
2022-07-28 04:58:00 -0700

    some memory allocation modifications done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,7 +30,7 @@ void calc_product(int *a, int *b, int len1, int len2)
 	int *product;
 	int i, j, tmp;
 
-	product = malloc(sizeof(*product) * (len1 + len2 + 1));
+	product = malloc(sizeof(*product) * (len1 + len2));
 	if (product == NULL)
 	{
 		free(a);
@@ -38,19 +38,19 @@ void calc_product(int *a, int *b, int len1, int len2)
 		print_error();
 	}
 
-	product = memset(product, 0, len1 + len2 + 1);
+	product = memset(product, 0, len1 + len2);
 	for (i = 0; i < len2; i++)
 		for (j = 0; j < len1; j++)
 			product[i + j] += b[i] * a[j];
 
-	for (i = 0; i < len1 + len2; i++)
+	for (i = 0; i < len1 + len2 - 1; i++)
 	{
 		tmp = product[i] / 10;
 		product[i] %= 10;
 		product[i + 1] += tmp;
 	}
 
-	for (i = len1 + len2; i >= 0; i--)
+	for (i = len1 + len2 - 1; i >= 0; i--)
 		if (product[i] > 0)
 			break;
 
@@ -67,6 +67,36 @@ void calc_product(int *a, int *b, int len1, int len2)
 }
 
 /**
+ * reverse_num_string - reverses a number string, but throws error if
+ * a non-digit character is found.
+ *
+ * @numstr: The number string.
+ * @len: The length of the number string.
+ * Return: The reversed number string, if successful.
+ */
+int *reverse_num_string(char *numstr, int len)
+{
+	int *num;
+	int i, j;
+
+	num = malloc(sizeof(*num) * len);
+	if (num == NULL)
+		return (NULL);
+
+	for (i = len - 1, j = 0; i >= 0; i--, j++)
+	{
+		if (numstr[i] < '0' || numstr[i] > '9')
+		{
+			free(num);
+			return (NULL);
+		}
+		num[j] = numstr[i] - '0';
+	}
+
+	return (num);
+}
+
+/**
  * main - multiplies two positive numbers, and prints the result.
  * @argc: The number of arguments.
  * @argv: The array of arguments.
@@ -74,41 +104,29 @@ void calc_product(int *a, int *b, int len1, int len2)
  */
 int main(int argc, char *argv[])
 {
-	int i, j, len1, len2;
+	int len1, len2;
 	int *a, *b;
 
-	if (argc != 3)
+	if (argc != 3 || argv[1] == NULL || argv[2] == NULL)
 		print_error();
 
 	len1 = strlen(argv[1]);
 	len2 = strlen(argv[2]);
 
-	a = malloc(sizeof(*a) * len1);
+	if (len1 == 0 || len2 == 0)
+		print_error();
+
+	a = reverse_num_string(argv[1], len1);
 	if (a == NULL)
 		print_error();
 
-	for (i = len1 - 1, j = 0; i >= 0; i--, j++)
-	{
-		if (argv[1][i] < '0' || argv[1][i] > '9')
-			print_error();
-
-		a[j] = argv[1][i] - '0';
-	}
-
-	b = malloc(sizeof(*b) * len2);
+	b = reverse_num_string(argv[2], len2);
 	if (b == NULL)
 	{
 		free(a);
 		print_error();
 	}
 
-	for (i = len2 - 1, j = 0; i >= 0; i--, j++)
-	{
-		if (argv[2][i] < '0' || argv[2][i] > '9')
-			print_error();
-
-		b[j] = argv[2][i] - '0';
-	}
 	calc_product(a, b, len1, len2);
 	return (0);
 }
