182f33039eb4378426ca2e36947b928062669339
Romha Shwangzaw <romhas@gmail.com>
2022-08-01 07:34:04 -0700

    new_dog function added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - creates a new dog.
@@ -12,22 +11,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *name_copy, *owner_copy;
+	int lenname, lenowner, i;
 
+	for (lenname = 0; name[lenname] != '\0'; lenname++)
+		;
+	lenname++;
+	for (lenowner = 0; owner[lenowner] != '\0'; lenowner++)
+		;
+	lenowner++;
+	name_copy = malloc(sizeof(*name_copy) * lenname);
+	if (name_copy == NULL)
+		return (NULL);
+	owner_copy = malloc(sizeof(*owner_copy) * lenowner);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		return (NULL);
+	}
 	d = malloc(sizeof(*d));
 	if (d == NULL)
+	{
+		free(name_copy);
+		free(owner_copy);
 		return (NULL);
+	}
 
-	d->name = malloc(sizeof(name));
-	if (d->name == NULL)
-		return (NULL);
+	for (i = 0; i < lenname; i++)
+		name_copy[i] = name[i];
 
-	d->owner = malloc(sizeof(owner));
-	if (d->owner == NULL)
-		return (NULL);
+	for (i = 0; i < lenowner; i++)
+		owner_copy[i] = owner[i];
 
-	strcpy(d->name, name);
-	strcpy(d->owner, owner);
-
+	d->name = name_copy;
 	d->age = age;
+	d->owner = owner_copy;
 	return (d);
 }
