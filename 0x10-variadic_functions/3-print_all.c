2087c0c26ffb2eca0c9caf9032d9ba1ef46e8bb7
Romha Shwangzaw <romhas@gmail.com>
2022-08-04 03:59:23 -0700

    print_all function added

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -51,7 +51,7 @@ void print_all(const char * const format, ...)
 				printf("%c", va_arg(ap, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(ap, int));
+				printf("%i", va_arg(ap, int));
 				break;
 			case 'f':
 				printf("%f", va_arg(ap, double));
