2182e13893ac3547f897b441f9eebe4793100030
Romha Shwangzaw <romhas@gmail.com>
2022-08-04 05:17:23 -0700

    print_all function added

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,28 +2,6 @@
 #include <stdio.h>
 
 /**
- * place_separator - places a separator, if needed.
- * @i: The format string's current index.
- * @c: The format string's current character.
- * Return: Nothing.
- */
-void place_separator(unsigned int i, char c)
-{
-	char *type = "cifs";
-	unsigned int j = 0;
-
-	while (type[j] && i)
-	{
-		if (c == type[j])
-		{
-			printf(", ");
-			return;
-		}
-		j++;
-	}
-}
-
-/**
  * print_all - prints anything.
  * @format: A list of types of arguments passed to the function:
  * c: char
@@ -38,33 +16,33 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	va_list ap;
-	char *str;
+	char *str, *sep = "";
 
 	va_start(ap, format);
 	while (format && format[i])
 	{
-		place_separator(i, format[i]);
-
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(ap, int));
+				printf("%s%c", sep, va_arg(ap, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(ap, int));
+				printf("%s%d", sep, va_arg(ap, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(ap, double));
+				printf("%s%f", sep, va_arg(ap, double));
 				break;
 			case 's':
 				str = va_arg(ap, char *);
 				if (str == NULL)
 					str = "(nil)";
-				printf("%s", str);
+				printf("%s%s", sep, str);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
+		sep = ", ";
 		i++;
 	}
 
