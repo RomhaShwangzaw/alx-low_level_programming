a9994c3b001b7e53694969b12a7f0a93510e250e
Romha Shwangzaw <romhas@gmail.com>
2022-08-11 11:41:29 -0700

    print_binary function added

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: The number to be converted to binary.
+ * Return: Nothing.
+ *
+ * Description: 
+ * - You are not allowed to use arrays
+ * - You are not allowed to use malloc
+ * - You are not allowed to use the % or / operators
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	(n & 1) ? _putchar('1') : _putchar('0');
+}
