70ad596ef98a551b4277cbdbaba80300dc277379
Romha Shwangzaw <romhas@gmail.com>
2022-08-15 11:34:49 -0700

    find_loop function added

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: The head of the list, to be checked for a loop.
+ * Return: The address of the node where the loop starts,
+ *			or NULL if there is no loop.
+ *
+ * Description:
+ * - You are not allowed to use malloc, free or arrays.
+ * - You can only declare a maximum of two variables in your function.
+ *
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tmp = NULL;
+
+	for (tmp = head; tmp; tmp = tmp->next)
+	{
+		if (tmp <= tmp->next)
+			return (tmp->next);
+	}
+
+	return (NULL);
+}
