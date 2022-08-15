8a093af5944f0dd1deeb466250b1ba3def1dc192
Romha Shwangzaw <romhas@gmail.com>
2022-08-15 09:33:33 -0700

    print_listint_safe function added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,6 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (head->next && (head <= head->next))
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			count++;
 			return (count);
 		}
 	}
