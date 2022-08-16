7723bb5af73646d6191fa7eac8d69768659e936f
Romha Shwangzaw <romhas@gmail.com>
2022-08-16 03:06:24 -0700

    print_listint_safe function added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,46 @@
 #include "lists.h"
 
 /**
+ * new_node - adds a new node at the start of a linked list.
+ * @head: The address of the start of the list.
+ * @add: The data to be inserted into the new node.
+ * Return: A pointer to the new node, or NULL on failure.
+ */
+listadd_t *new_node(listadd_t **head, listint_t *add)
+{
+	listadd_t *node;
+
+	node = malloc(sizeof(*node));
+	if (node == NULL)
+		return (NULL);
+
+	node->add = add;
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
+
+/**
+ * check_looplist - checks if a linked list has a loop.
+ * @head: The start of the list.
+ * @node: The node to be checked for repetition/loop.
+ * Return: 1 if there is a loop, 0 otherwise.
+ */
+unsigned int check_looplist(listadd_t *head, listint_t *node)
+{
+	listadd_t *h;
+
+	for (h = head; h; h = h->next)
+	{
+		if (h->add == node)
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
  * print_listint_safe - prints a listint_t linked list.
  * @head: The list to be printed.
  * Return: The number of nodes in the list.
@@ -14,15 +54,28 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
+	listadd_t *start = NULL, *tmp = NULL;
+	listint_t *h = NULL;
 
-	for (; head; head = head->next)
+	if (head == NULL)
+		return (0);
+
+	printf("[%p] %d\n", (void *)head, head->n);
+	count++;
+
+	for (h = head->next; h; h = h->next)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
-
-		if (head <= head->next)
+		if ((h != head) && (check_looplist(start, h) == 0))
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("[%p] %d\n", (void *)h, h->n);
+			count++;
+			tmp = new_node(&start, h);
+			if (tmp == NULL)
+				exit(98);
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)h, h->n);
 			return (count);
 		}
 	}
