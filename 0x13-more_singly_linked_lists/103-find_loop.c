1ab115cd51b8d04598e7e3073052c3bf28ca47e4
Romha Shwangzaw <romhas@gmail.com>
2022-08-16 03:15:43 -0700

    find_loop function added

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,24 +1,40 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list.
- * @head: The head of the list, to be checked for a loop.
- * Return: The address of the node where the loop starts,
- *			or NULL if there is no loop.
+ * find_listint_loop - Finds the loop contained in
+ *                     a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
  *
- * Description:
- * - You are not allowed to use malloc, free or arrays.
- * - You can only declare a maximum of two variables in your function.
- *
+ * Return: If there is no loop - NULL.
+ *         Otherwise - the address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tmp = NULL;
+	listint_t *tortoise, *hare;
 
-	for (tmp = head; tmp; tmp = tmp->next)
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		if (tmp <= tmp->next)
-			return (tmp->next);
+		if (tortoise == hare)
+		{
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			return (tortoise);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 
 	return (NULL);
