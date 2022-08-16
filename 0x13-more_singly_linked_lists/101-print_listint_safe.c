83cfd27d84d80f3f349968fd0222fdb11e16c9db
Romha Shwangzaw <romhas@gmail.com>
2022-08-16 03:12:19 -0700

    print_listint_safe function added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,84 +1,84 @@
 #include "lists.h"
 
 /**
- * new_node - adds a new node at the start of a linked list.
- * @head: The address of the start of the list.
- * @add: The data to be inserted into the new node.
- * Return: A pointer to the new node, or NULL on failure.
+ * looped_listint_len - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
  */
-listadd_t *new_node(listadd_t **head, listint_t *add)
+size_t looped_listint_len(const listint_t *head)
 {
-	listadd_t *node;
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-	node = malloc(sizeof(*node));
-	if (node == NULL)
-		return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-	node->add = add;
-	node->next = *head;
-	*head = node;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-	return (node);
-}
-
-/**
- * check_looplist - checks if a linked list has a loop.
- * @head: The start of the list.
- * @node: The node to be checked for repetition/loop.
- * Return: 1 if there is a loop, 0 otherwise.
- */
-unsigned int check_looplist(listadd_t *head, listint_t *node)
-{
-	listadd_t *h;
-
-	for (h = head; h; h = h->next)
+	while (hare)
 	{
-		if (h->add == node)
-			return (1);
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 
 	return (0);
 }
 
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: The list to be printed.
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
  * Return: The number of nodes in the list.
- *
- * Description:
- * - This function can print lists with a loop.
- * - You should go through the list only once.
- * - If the function fails, exit the program with status 98.
- *
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	listadd_t *start = NULL, *tmp = NULL;
-	listint_t *h = NULL;
+	size_t nodes, index = 0;
 
-	if (head == NULL)
-		return (0);
+	nodes = looped_listint_len(head);
 
-	printf("[%p] %d\n", (void *)head, head->n);
-	count++;
-
-	for (h = head->next; h; h = h->next)
+	if (nodes == 0)
 	{
-		if ((h != head) && (check_looplist(start, h) == 0))
+		for (; head != NULL; nodes++)
 		{
-			printf("[%p] %d\n", (void *)h, h->n);
-			count++;
-			tmp = new_node(&start, h);
-			if (tmp == NULL)
-				exit(98);
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)h, h->n);
-			return (count);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
-	return (count);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,20 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct listadd_s - singly linked list
- * @add: listint_t pointer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
- */
-typedef struct listadd_s
-{
-	listint_t *add;
-	struct listadd_s *next;
-} listadd_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
