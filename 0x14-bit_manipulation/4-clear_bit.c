d16c3d0fa88e968752fe5abd8004f69fe9fee475
Romha Shwangzaw <romhas@gmail.com>
2022-08-18 02:31:33 -0700

    Fixed invalid index checking

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int bit;
 
+	/* Check for index out of range */
+	if (index >= (sizeof(*n) * 8))
+		return (-1);
+
 	bit = 1 << index;
 	*n |= bit;
 	*n ^= bit;
