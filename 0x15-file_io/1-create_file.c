7785bea85dc620f05511e540be0b1de7bfb0a91a
Romha Shwangzaw <romhas@gmail.com>
2022-08-22 03:50:57 -0700

    Minor fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,9 +22,13 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd == -1)
-		return (-1);
+	{
+		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+		if (fd == -1)
+			return (-1);
+	}
 
 	for (len = 0; text_content[len];)
 		len++;
