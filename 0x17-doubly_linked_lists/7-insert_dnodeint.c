df0270f2485d0f29c2ad7455c1c5392946d3d576
Romha Shwangzaw <romhas@gmail.com>
2022-09-29 03:24:10 -0700

    edge cases taken care of

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,29 +17,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *head = *h, *new;
 	unsigned int counter = 0;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
-	if (*h == NULL && idx == 0)
-	{
-		*h = new;
-		return (new);
-	}
 
 	while (head)
 	{
 		if (counter == idx)
 		{
+			new->n = n;
 			new->prev = head->prev;
 			head->prev = new;
 			new->next = head;
-			if (new->prev != NULL)
-				new->prev->next = new;
+			new->prev->next = new;
 			return (new);
 		}
 		counter++;
@@ -47,5 +40,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	free(new);
+
+	if (counter == idx)
+		return (add_dnodeint_end(h, n));
+
 	return (NULL);
 }
