a465d7221334208487e5d2d994b9daae6895c5be
Romha Shwangzaw <romhas@gmail.com>
2022-09-29 03:16:16 -0700

    Minor fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = head->prev;
 			head->prev = new;
 			new->next = head;
-			new->prev->next = new;
+			if (new->prev != NULL)
+				new->prev->next = new;
 			return (new);
 		}
 		counter++;
