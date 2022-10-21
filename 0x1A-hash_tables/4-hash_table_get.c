867696a8d3e36a181b21578724841caf1d61bd07
Romha Shwangzaw <romhas@gmail.com>
2022-10-21 04:55:16 -0700

    Minor fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,12 +10,12 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	    key == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	while (node)
 	{
