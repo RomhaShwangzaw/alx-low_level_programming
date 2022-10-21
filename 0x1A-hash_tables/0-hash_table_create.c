5571797147ea891a1541ef14b3a9dd3423b4f6a9
Romha Shwangzaw <romhas@gmail.com>
2022-10-21 02:43:34 -0700

    initialized array elements to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
@@ -21,7 +22,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(table);
 		return (NULL);
 	}
-	memset(table->array, 0, sizeof(hash_node_t *) * size);
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
