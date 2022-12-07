63e5a72d7e8e8fce468d14ea8138805e8cb73ae1
Romha Shwangzaw <romhas@gmail.com>
2022-12-07 16:21:36 +0300

    Bug Fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains a function to calculate the perimeter of an island."""
 
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid:
 
@@ -10,17 +11,23 @@ def island_perimeter(grid):
         - One cell is a square with side length 1
         - Grid cells are connected horizontally/vertically (not diagonally).
         - Grid is rectangular, width and height don’t exceed 100
-    Grid is completely surrounded by water, and there is one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+    Grid is completely surrounded by water, and there is one island
+    (or nothing).
+    The island doesn’t have “lakes” (water inside that isn’t connected to the
+    water around the island).
     """
-    
-    count = 0
-    for r in grid:
-        for c in r:
-            if c == 1:
-                count += 1
 
-    if count != 0:
-        return (count + 1) * 2
-    else:
-        return count
+    width = len(grid[0])
+    height = len(grid)
+    size = 0
+    edges = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges += 1
+    return size * 4 - edges * 2
