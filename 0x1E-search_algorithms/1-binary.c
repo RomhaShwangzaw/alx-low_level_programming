6398c8212245f1fe4d041c6b7a7694d15dbd4463
Romha Shwangzaw <romhas@gmail.com>
2023-01-28 01:36:14 +0300

    binary search algorithm added

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,57 @@
+#include "search_algos.h"
+
+/**
+ * binary_search_recursive - searches for a value in an array of integers
+ * recursively using the Binary search algorithm.
+ *
+ * @array: an array of integers to search the value in.
+ * @start: the starting index of the search.
+ * @end: the last index of the search.
+ * @value: the value to search for.
+ *
+ * Return: the first index where value is located, or -1 if
+ * value is not present in array or if array is NULL.
+ *
+ * Description: It will print the array being searched every time it changes.
+ */
+int binary_search_recursive(int *array, int start, int end, int value)
+{
+	int mid, i;
+
+	if (start >= end)
+		return (-1);
+
+	printf("Searching in array:");
+	for (i = start; i < end - 1; i++)
+		printf(" %d,", array[i]);
+	printf(" %d\n", array[end - 1]);
+
+	mid = (end + start) / 2;
+	if (array[mid] == value)
+		return (mid);
+	else if (array[mid - 1] >= value)
+		return (binary_search_recursive(array, start, mid, value));
+	else
+		return (binary_search_recursive(array, mid + 1, end, value));
+}
+
+/**
+ * binary_search - searches for a value in an array of integers
+ * using the Binary search algorithm.
+ *
+ * @array: a pointer to the first element of the array to search in.
+ * @size: the number of elements in array.
+ * @value: the value to search for.
+ *
+ * Return: the first index where value is located, or -1 if
+ * value is not present in array or if array is NULL.
+ *
+ * Description: It will print the array being searched every time it changes.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (size == 0)
+		return (-1);
+
+	return (binary_search_recursive(array, 0, size, value));
+}
